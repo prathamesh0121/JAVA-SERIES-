5e749b7d37287bf07e73b605acd904696d975d50
Prathamesh Tambekar <prathameshtambekar9425@gmail.com>
2025-08-20 11:00:16 +0530

    Add files via upload

---
+++ AdvCalculator.java
@@ -0,0 +1,13 @@
+public class AdvCalculator extends Calculator   //inherit AdvCalculator in the Calculator class and access the properties.
+{                                               // single level inheritance
+    public int mul(int a,int b)
+    {
+        return a* b;
+    }
+    public int div(int a, int b)
+    {
+        return a/b;
+    }
+}
+
+
